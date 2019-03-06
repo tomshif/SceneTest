2be1b4d88f58a308388e737af381bedbca6edced
Tom Shiflet <tom.shiflet@mylcs.com>
2019-03-06 09:55:11 -0600

    Initial Commit

---
+++ SceneTest/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  SceneTest
+//
+//  Created by Tom Shiflet on 3/6/19.
+//  Copyright © 2019 Tom Shiflet. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+}
